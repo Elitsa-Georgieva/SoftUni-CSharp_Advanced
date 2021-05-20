213edef1245ada934dc14ed6f5c735dd1ea2da71
Elitsa-Georgieva <itsa.georgieva@gmail.com>
2021-05-20 11:02:16 +0300

    Added one new program
    One program added to folder Stacks and Queues

---
+++ Stacks and Queues/8.TrafficJam/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8.TrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int totalCarsPassed = 0;
+            Queue<string> cars = new Queue<string>();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command != "end" && command != "green")
+                {
+                    cars.Enqueue(command);
+                }
+
+                if (command == "end")
+                {
+                    Console.WriteLine($"{totalCarsPassed} cars passed the crossroads");
+                    break;
+                }
+
+                if (command == "green")
+                {
+                    
+                    for (int i = 0; i < N; i++)
+                    { 
+                        if (cars.Count == 0)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{cars.Dequeue()} passed!");
+                            totalCarsPassed += 1;
+                        }
+                            
+                    }
+                    
+                }
+                //else
+                //{
+                //    cars.Enqueue(command);
+                //    Console.WriteLine($"{command} passed!");
+                //}
+            }
+        }
+    }
+}
