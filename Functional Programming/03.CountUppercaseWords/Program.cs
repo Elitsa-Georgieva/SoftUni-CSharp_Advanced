33ab9d898ac8a534a0ab21bbe7a3e29d0aeda421
Elitsa-Georgieva <itsa.georgieva@gmail.com>
2021-06-04 17:02:31 +0300

    Added 5 new programs
    Added to Functional Programming folder

---
+++ Functional Programming/03.CountUppercaseWords/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<string> predicate = s => s[0] == s.ToUpper()[0];
+            //СЪЩОТО!!! Predicate<string> predicate = s => char.IsUpper(s[0]);
+            
+            //взимаме първата буква от стринга и проверяваме дали е равна на първата буква от стринга като главна
+
+            //СЪЩОТО!!! като Func<string, bool> predicate = s => s[0] == s.ToUpper()[0];
+
+            //Predicate е функция (и делегат), която получава някакви параметри и връща bool
+
+            Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                //Where очаква Predicate по принцип
+                .Where(w => predicate(w))
+                .ToList()
+                .ForEach(w => Console.WriteLine(w));
+
+           
+        }
+    }
+}
