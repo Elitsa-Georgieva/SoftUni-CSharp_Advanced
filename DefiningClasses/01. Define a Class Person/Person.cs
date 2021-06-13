6df91ad52048aaebbd6a7f3a2d51f20b1a8f3300
Elitsa-Georgieva <itsa.georgieva@gmail.com>
2021-06-13 15:05:31 +0300

    Added 1 program to Defining classes folder
    Added 1 program to Defining classes folder

---
+++ DefiningClasses/01. Define a Class Person/Person.cs
@@ -0,0 +1,27 @@
+﻿
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person()
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int Age
+        {
+            get => this.age;
+            set => this.age = value;
+        }
+    }
+}
---
+++ DefiningClasses/01. Define a Class Person/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person pesho = new Person()
+            {
+                Name = "Pesho",
+                Age = 20
+            };
+
+
+            Console.WriteLine($"{pesho.Name} {pesho.Age}");
+
+
+        }
+    }
+}
